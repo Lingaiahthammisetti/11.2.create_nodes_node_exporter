89b7e8778caad2cedbefed061dc89325f6a9fb2b
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-05-15 00:10:43 +0530

    nodes installation

---
+++ main.tf
@@ -0,0 +1,21 @@
+
+resource "aws_instance" "nodes_ec2" {
+    for_each = var.instances
+    ami           = data.aws_ami.rhel_info.id
+    instance_type = each.value
+    vpc_security_group_ids = [var.allow_everything]
+    #user_data = file("${path.module}/node_exporter/node_exporter.sh")
+
+    tags = {
+        Name = each.key
+    }
+}
+resource "aws_route53_record" "nodes_r53" {
+    zone_id = var.zone_id
+    for_each = aws_instance.nodes_ec2
+    name    = "${each.key}.${var.domain_name}"
+    type    = "A"
+    ttl     = 1
+    records = [aws_instance.nodes_ec2.public_ip]
+    allow_overwrite = true
+}
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "allow_everything" {
+   type = string
+   default = "sg-09c7c70bd56f0d58b"
+}
+
+variable "instances" {
+    type     = map
+    default = {
+       node-1     = "t3.micro"
+      #  node-2     = "t3.micro"
+      #  node-3     = "t3.micro"
+    }
+}
+variable "zone_id" {
+    default = "Z08032413NTE19HSX8KA1"
+}
+variable "domain_name" {
+  default = "lingaiah.online"
+}
+
+
