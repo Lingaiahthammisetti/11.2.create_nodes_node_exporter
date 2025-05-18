f233492aff0f6b513996347061c1f343b9371cf6
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-05-18 20:32:33 +0530

    create node exporter

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "aws_route53_record" "nodes_r53" {
     name    = "${each.key}.${var.domain_name}"
     type    = "A"
     ttl     = 1
-    records = [aws_instance.nodes_ec2.public_ip]
+    records = each.key == "" ? [] : [each.value.public_ip]
     allow_overwrite = true
 }
