ae86b1d97ecd9016009654315b6072ee5e1f5c7e
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-07 23:00:55 +0530

    create node exporter

--- variables.tf
+++ variables.tf
@@ -6,8 +6,10 @@ variable "instances" {
     type     = map
     default = {
        node-1     = "t3.micro"
-       node-2     = "t3.micro"
+      #  node-2     = "t3.micro"
       #  node-3     = "t3.micro"
+      #  node-4     = "t3.micro"
+      #  node-5     = "t3.micro"
     }
 }
 variable "zone_id" {
